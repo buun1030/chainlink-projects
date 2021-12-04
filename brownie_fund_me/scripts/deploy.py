964dc7585fc81d77a239af6e85087fd55f65103a
buun1030 <boramattangwiwat@gmail.com>
2021-12-04 11:05:21 +0700

    brownie

---
+++ brownie_fund_me/scripts/deploy.py
@@ -0,0 +1,33 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+    # pass the price feed address to our fundme contract
+
+    # if we are on persistent network like ropsten, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
